40568213d151b976eda3931d3453f11052a8aef9
Jonathan Claus <jonathanmclaus@gmail.com>
2014-12-12 16:08:35 -0500

    Merged Stop, removed arguments, automated error handling

--- R/debug.R
+++ R/debug.R
@@ -1,3 +1,21 @@
+Stop <- function() {
+  if (exists("grokit.jobid")) {
+    tb <- lapply(head(tail(sys.calls(), -4), -1), deparse)
+    tr <- list(line = 1, col = 1, line.end = 1, col.end = 1)
+    error <- list(
+        `__type__` = "error",
+        message = geterrmessage(),
+        kind = "R",
+        traceback = tb,
+        line = tr$line,
+        column = tr$col,
+        line_end = tr$line.end,
+        column_end = tr$col.end
+        )
+    throw.error(error)
+  }
+}
+
 Traceback <- function() {
   tr <- .Internal(traceback(0))
   err <- tr[[tail(which(!as.logical(lapply(lapply(tr, attr, "srcref"), is.null))), 1)]]
