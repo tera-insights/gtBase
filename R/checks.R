2702ef5e73eea337f731830118f539c1bc44ce2b
Jonathan Claus <jonathanmclaus@gmail.com>
2014-12-12 16:07:03 -0500

    Fixed check.inputs symbol to character translation, updated stop

--- R/checks.R
+++ R/checks.R
@@ -1,35 +1,28 @@
-check.alias <- function(alias) {
-  if (alias %in% grokit$waypoints)
-    alias <- get.alias(strsplit(alias, "_", TRUE)[[1]][[1]])
-  grokit$waypoints <- c(grokit$waypoints, alias)
-  alias
-}
-
 ## attributes should be language
 check.atts <- function(attributes, auto = TRUE) {
   if (is.call.to(attributes, "c") && !all(is.attributes(as.list(attributes)[-1])))
-    Stop("attributes specified incorrectly.")
+    stop("attributes specified incorrectly.")
   else if (!is.call.to(attributes, "c") && !is.attributes(list(attributes)))
-    Stop("attribute specified incorrectly.")
+    stop("attribute specified incorrectly.")
   else if (is.auto(attributes) && !auto)
-    Stop("AUTO used illegally.")
+    stop("AUTO used illegally.")
 }
 
 check.exprs <- function(expressions, auto = TRUE) {
   if (!(length(expressions) == 1 || is.call(expressions)))
-    Stop("expressions specified incorrectly.")
+    stop("expressions specified incorrectly.")
   else if (is.auto(expressions) && !auto)
-    Stop("AUTO used illegally.")
+    stop("AUTO used illegally.")
 }
 
 check.inputs <- function(x, schema) {
   expressions <- grokit$expressions[schema]
   atts <- unlist(lapply(expressions, function(expression) {
-    as.character(extract.symbols(expression))
+    unlist(lapply(extract.symbols(expression), as.character))
   }))
   missing <- subtract(atts, names(x$schema))
   if (length(missing) != 0)
-    Stop("the following attributes are missing:\n",
+    stop("the following attributes are missing:\n",
          paste0("\t", missing, collapse = "\n"))
 }
 
@@ -41,5 +34,5 @@ check.missing <- function() {
 check.schema <- function(x, schema) {
   atts <- x$schema[schema]
   if (any(bad <- !(schema) %in% names(x$schema)))
-    Stop("missing attributes: ", paste(schema[bad], collapse = ", "))
+    stop("missing attributes: ", paste(schema[bad], collapse = ", "))
 }
