8a573431dcaa8a5ec20efe69c8e6cb15d80ac439
Jonathan Claus <jonathanmclaus@gmail.com>
2014-12-12 16:10:02 -0500

    Updated stop

--- R/filter.R
+++ R/filter.R
@@ -3,11 +3,11 @@
 `[.data` <- function(data, condition) {
   condition <- substitute(condition)
   if (condition[[1]] == "c")
-    Stop("Condition is not allowed to be a listt of expressions.")
+    stop("Condition is not allowed to be a listt of expressions.")
   check.exprs(condition)
   condition <- convert.exprs(condition, data)
   check.inputs(data, condition)
-  alias <- get.alias("filter")
+  alias <- create.alias("filter")
   schema <- data$schema
   filter <- list(data = data, alias = alias, schema = schema, condition = condition)
   class(filter) <- c("Filter", "data")
