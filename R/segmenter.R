60b01bc656fe646d5d754ff2d001f461af1c8d2b
Jonathan Claus <jonathanmclaus@gmail.com>
2015-12-08 17:10:22 -0500

    simplified class checking

--- R/segmenter.R
+++ R/segmenter.R
@@ -1,7 +1,6 @@
 Segmenter <- function(data, segment, passes = 1, num.segments = 64) {
-  if (!("GLA" %in% class(data)))
+  if (!is(data, "GLA"))
     stop("Segment must be placed on top of an aggregate waypoint.")
-
   if (missing(segment))
     if (length(data$inputs) == 0)
       stop("Segment cannot be AUTO as the input GLA has no inputs.")
