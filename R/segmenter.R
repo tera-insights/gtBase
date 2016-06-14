69ec65c1a52747e3aa1d79442514ee29267f9c8f
Jonathan Claus <jonathanmclaus@gmail.com>
2016-06-14 14:46:45 -0400

    Added inner.GLA argument

--- R/segmenter.R
+++ R/segmenter.R
@@ -1,6 +1,14 @@
-Segmenter <- function(data, segment, passes = 1, segments = 64, inner.prefer.fragment = FALSE) {
-  if (!is(data, "GLA"))
+## If inner.GLA is given, then it should be a call specifying another waypoint
+## but without a data argument. Otherwise, the data given is taken to a GLA
+## waypoint and the Segmenter is placed on top of it.
+Segmenter <- function(data, segment, passes = 1, segments = 64, inner.prefer.fragment = FALSE, inner.GLA) {
+  if (!missing(inner.GLA)) {
+    inner.GLA <- substitute(inner.GLA)
+    inner.GLA[["data"]] <- data
+    data <- eval(inner.GLA)
+  } else if (!is(data, "GLA")) {
     stop("Segment must be placed on top of an aggregate waypoint.")
+  }
   if (missing(segment))
     if (length(data$inputs) == 0)
       stop("Segment cannot be AUTO as the input GLA has no inputs.")
